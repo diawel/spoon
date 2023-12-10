4a5038791e14b756dd2a8fc9b55e01920597d359
Diawel <38136957+diawel@users.noreply.github.com>
2023-12-10 23:07:55 +0900

    init: prettierを設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import styles from "./page.module.css";
+import styles from './page.module.css'
 
 export default function Home() {
-  return <main className={styles.main}>ここは/です</main>;
+  return <main className={styles.main}>ここは/です</main>
 }
