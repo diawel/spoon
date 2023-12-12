c28f6e1beb82113486e5cdd86360d96a1ab42e84
Diawel <38136957+diawel@users.noreply.github.com>
2023-12-12 21:43:54 +0900

    add: DBを整備

--- src/layouts/Today/index.tsx
+++ src/layouts/Today/index.tsx
@@ -1,6 +1,31 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import FinderView from './FinderView'
+import { history } from '@/utils/history'
 
 const Today: React.FC = () => {
+  const [captureData, setCaptureData] = useState<{
+    date: Date
+    image: Blob
+    flavor?: string
+    colors: { r: number; g: number; b: number }[]
+  } | null>(null)
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    const now = new Date()
+    history.days
+      .get(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`)
+      .then((today) => {
+        if (today) setCaptureData(today)
+        else setCaptureData(null)
+        setReady(true)
+      })
+  }, [])
+
+  if (!ready) return null
+  if (captureData) return <div>TODO: show capture data</div>
   return <FinderView />
 }
 
---
+++ src/utils/history.ts
@@ -0,0 +1,24 @@
+'use client'
+
+import Dexie, { Table } from 'dexie'
+
+export type Day = {
+  dateString: string // YYYY-MM-DD
+  date: Date
+  image: Blob
+  flavor: string
+  colors: { r: number; g: number; b: number }[]
+}
+
+class History extends Dexie {
+  days!: Table<Day>
+
+  constructor() {
+    super('History')
+    this.version(1).stores({
+      days: 'date',
+    })
+  }
+}
+
+export const history = new History()
