5c5c22e45605589c6356ecf65a64cf31cd5f5091
addtobasic <addtobasic@gmail.com>
2023-12-14 01:17:23 +0900

    fix TabViewButtonの削除

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -3,7 +3,6 @@ import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import { iceCellData } from './DummyData'
 
-import TabViewButton from './components/TabViewButton'
 import TodayIceButton from './components/TodayIceButton'
 
 import styles from './page.module.css'
@@ -31,11 +30,6 @@ export default function Home() {
       </div>
       <div className={styles.photoViewContainer}>
         <PhotoView selectedElement={selectedElement} />
-      </div>
-      <div className={styles.tabViewButtonContainer}>
-        <div className={styles.rightAligned}>
-          <TabViewButton />
-        </div>
       </div>
       <div className={styles.fridgeContainer}>
         <FridgeView onIceCellTap={handleIceCellTap} historyData={iceCellData} />
