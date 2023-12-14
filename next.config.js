9a181e4d39d16f95c8c7136f01647d6a0f7c2c0a
Diawel <38136957+diawel@users.noreply.github.com>
2023-12-14 20:58:09 +0900

    clean: next-pwaが不要なので削除

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-})
-
 module.exports = nextConfig
