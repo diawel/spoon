06983f4be92a4f43e6fc6b43df6c62c78e2e5b2e
addtobasic <addtobasic@gmail.com>
2023-12-15 23:57:57 +0900

    wip: セルのsvg表示の途中経過までを共有

--- src/layouts/History/Fridge/IceCell/IceCell.tsx
+++ src/layouts/History/Fridge/IceCell/IceCell.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './IceCell.module.css'
 import { Ice } from '@/utils/history'
+import { base, patterns } from './parts'
 
 export type IceCellProps = {
   iceCell: Ice
@@ -12,14 +13,38 @@ const IceCell: React.FC<IceCellProps> = ({ iceCell, onIceCellTap }) => {
     onIceCellTap(iceCell)
   }
 
+  const rgbList = iceCell.colors.map(
+    (color) => `rgb(${color.r}, ${color.g}, ${color.b})`
+  )
+
   return (
     <div onClick={handleTap} className={styles.iceCellContainer}>
       <div className={styles.iceCell}>
+        <div>
+          <IceCellSvg color={rgbList[0]}>{base}</IceCellSvg>
+          <IceCellSvg color={rgbList[1]}>
+            {patterns[iceCell.pattern][0]}
+          </IceCellSvg>
+          <IceCellSvg color={rgbList[2]}>
+            {patterns[iceCell.pattern][1]}
+          </IceCellSvg>
+        </div>
         {/* 月全体で最後の月の場合にのみ下の要素を当てる */}
         <div className={styles.month}>{iceCell.date.getMonth() + 1}月</div>
       </div>
     </div>
   )
 }
+
+type IceCellSvgProps = {
+  children: React.ReactNode
+  color: string
+}
+
+const IceCellSvg: React.FC<IceCellSvgProps> = ({ children, color }) => (
+  <svg className={styles.parts} viewBox="0 0 128 128" fill={color}>
+    {children}
+  </svg>
+)
 
 export default IceCell
