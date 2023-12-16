d62133bbfb8a980e100ae421ae069609076b4ca5
addtobasic <addtobasic@gmail.com>
2023-12-16 10:35:22 +0900

    add: historyへの写真切替え時の時のアニメーションの追加

--- src/layouts/History/Photo/PhotoView.tsx
+++ src/layouts/History/Photo/PhotoView.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image'
 import React from 'react'
 import styles from './PhotoView.module.css'
 import { Ice } from '@/utils/history'
-        
+import { motion } from 'framer-motion'
+
 export type PhotoViewProps = {
   selectedElement: Ice | null
 }
@@ -14,51 +15,62 @@ const KaiseiTokuminFont = Kaisei_Tokumin({
   subsets: ['latin'],
 })
 
+const fadeInVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 1.0 } },
+}
+
 const PhotoView: React.FC<PhotoViewProps> = ({ selectedElement }) => {
   return (
     <div className={styles.container}>
       {selectedElement ? (
-        <div className={styles.photoContainer}>
-          <div className={styles.iceLayout}>
-            <Image
-              className={styles.photo}
-              src="/test.JPG"
-              width={240}
-              height={240}
-              alt="photo before ice cream"
-            />
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={fadeInVariants}
+        >
+          <div className={styles.photoContainer}>
+            <div className={styles.iceLayout}>
+              <Image
+                className={styles.photo}
+                src="/test.JPG"
+                width={240}
+                height={240}
+                alt="photo before ice cream"
+              />
 
+              <div className={styles.iceCreamContaner}>
+                <Icecream
+                  pattern={selectedElement.pattern}
+                  colors={selectedElement.colors}
+                />
+              </div>
+              <div className={styles.photoOverlay}>
+                <p
+                  className={`${styles.photo_month} ${KaiseiTokuminFont.className}`}
+                >
+                  {selectedElement.date.getMonth() + 1}月
+                </p>
+                <p
+                  className={`${styles.photo_day} ${KaiseiTokuminFont.className}`}
+                >
+                  {selectedElement.date.getDate()}
+                </p>
+              </div>
+              <p
+                className={`${styles.flavor_name} ${KaiseiTokuminFont.className}`}
+              >
+                {selectedElement.flavor}
+              </p>
+            </div>
             <div className={styles.iceCreamContaner}>
               <Icecream
                 pattern={selectedElement.pattern}
                 colors={selectedElement.colors}
               />
             </div>
-            <div className={styles.photoOverlay}>
-              <p
-                className={`${styles.photo_month} ${KaiseiTokuminFont.className}`}
-              >
-                {selectedElement.date.getMonth() + 1}月
-              </p>
-              <p
-                className={`${styles.photo_day} ${KaiseiTokuminFont.className}`}
-              >
-                {selectedElement.date.getDate()}
-              </p>
-            </div>
-            <p
-              className={`${styles.flavor_name} ${KaiseiTokuminFont.className}`}
-            >
-              {selectedElement.flavor}
-            </p>
           </div>
-          <div className={styles.iceCreamContaner}>
-            <Icecream
-              pattern={selectedElement.pattern}
-              colors={selectedElement.colors}
-            />
-          </div>
-        </div>
+        </motion.div>
       ) : (
         <div className={styles.photoContainer}>
           <Image
