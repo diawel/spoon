8cbe4bd4018e11ac2561ae5ada0e4a5fd71de4f5
addtobasic <addtobasic@gmail.com>
2023-12-16 10:31:23 +0900

    add: historyへの画面遷移時のアニメーションの追加

--- src/layouts/History/index.tsx
+++ src/layouts/History/index.tsx
@@ -8,6 +8,7 @@ import FridgeView from './Fridge/FridgeView'
 import { Ice } from '@/utils/history'
 import { useLiveQuery } from 'dexie-react-hooks'
 import { history } from '@/utils/history'
+import { motion } from 'framer-motion'
 
 const History: React.FC = () => {
   const [selectedElement, setSelectedElement] = useState<Ice | null>(null)
@@ -20,22 +21,28 @@ const History: React.FC = () => {
     }
   })
 
+  const fadeInVariants = {
+    hidden: { opacity: 0, y: 40 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1.0 } },
+  }
+
   if (!liveQuery) return <></>
   return (
     <main className={styles.main}>
       <div className={styles.todayIceButtonContainer}>
         <TodayIceButton />
       </div>
-      <div className={styles.photoViewContainer}>
-        <PhotoView selectedElement={selectedElement} />
-      </div>
-
-      <div className={styles.fridgeContainer}>
-        <FridgeView
-          onIceCellTap={handleIceCellTap}
-          historyData={liveQuery.days}
-        />
-      </div>
+      <motion.div initial="hidden" animate="visible" variants={fadeInVariants}>
+        <div className={styles.photoViewContainer}>
+          <PhotoView selectedElement={selectedElement} />
+        </div>
+        <div className={styles.fridgeContainer}>
+          <FridgeView
+            onIceCellTap={handleIceCellTap}
+            historyData={liveQuery.days}
+          />
+        </div>
+      </motion.div>
     </main>
   )
 }
